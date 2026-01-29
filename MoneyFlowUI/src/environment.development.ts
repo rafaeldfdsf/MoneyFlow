de849478e091861399924be3c3ae7e6039137af4
Rafael Rodrigues <rafael.r2001@gmail.com>
2026-01-29 19:26:30 +0000

    fix environment.development.ts

--- MoneyFlowUI/src/environment.development.ts
+++ MoneyFlowUI/src/environment.development.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
     production: true,
-    apiUrl: 'https://money-api-2026-ebdsadf8fqe9e6dm.canadacentral-01.azurewebsites.net'
+    apiUrl: 'https://money-api-2026-ebdsadf8fqe9e6dm.canadacentral-01.azurewebsites.net/api'
 };
