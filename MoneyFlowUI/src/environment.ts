53083d55773eff88158f0aadf874db2aadd8c701
Rafael Rodrigues <rafael.r2001@gmail.com>
2026-01-29 10:10:34 +0000

    environment

--- MoneyFlowUI/src/environment.ts
+++ MoneyFlowUI/src/environment.ts
@@ -1,4 +1,9 @@
+// export const environment = {
+//     production: true,
+//     apiUrl: 'https://localhost:7085/api' // ou o endereço real quando fores publicar
+// };
+
 export const environment = {
     production: true,
-    apiUrl: 'https://localhost:7085/api' // ou o endereço real quando fores publicar
+    apiUrl: 'https://money-api-2026-ebdsadf8fqe9e6dm.canadacentral-01.azurewebsites.net'
 };
